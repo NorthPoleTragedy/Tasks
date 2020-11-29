f03d884620638d012beb55a1042f828aa96b3563
Artyom Nyan <artemqwerty55@gmail.com>
2020-11-29 17:50:00 +0300

    init commit

---
+++ src/test/java/stackcalculator/LauncherTest.java
@@ -0,0 +1,12 @@
+package stackcalculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LauncherTest {
+    @Test
+    public void testTest(){
+        assertEquals(2*2, 4);
+    }
+}
