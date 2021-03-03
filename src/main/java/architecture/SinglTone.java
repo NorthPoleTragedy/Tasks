56166cdaca00b4fcdacd0e1c69a7a3d2b32a7e9f
Artyom Nyan <artemqwerty55@gmail.com>
2021-03-03 23:18:50 +0300

    made singleton

---
+++ src/main/java/architecture/SinglTone.java
@@ -0,0 +1,20 @@
+package architecture;
+
+public class SinglTone {
+    private final String abc;
+    static SinglTone singlTone;
+
+    private SinglTone(){
+        this.singlTone = new SinglTone();
+        abc = "OOO";
+    }
+
+    public static SinglTone getInstance(){
+        return singlTone;
+    }
+
+    public String typeOOO(){
+        return abc;
+    }
+
+}
